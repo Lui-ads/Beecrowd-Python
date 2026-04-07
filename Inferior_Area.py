4aeae465fe9b41049c74d401b443e2ba9449e4d6
MadridMiranda <madrid2005miranda@gmail.com>
2026-04-07 20:28:22 -0300

    Add files via upload

---
+++ Inferior_Area.py
@@ -0,0 +1,20 @@
+# Inferior Area
+
+o = input()  
+soma = 0.0
+contador = 0
+for k in range(144): 
+    valor = float(input()) 
+    i = k // 12
+    j = k % 12
+    if j < i and i + j > 11: 
+        soma += valor
+        contador += 1
+if o == 'S':
+    print("%.1f" % soma)
+elif o == 'M':
+    if contador > 0: 
+        media = soma / contador
+        print("%.1f" %media)
+    else:
+        print("0.0")
